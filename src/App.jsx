5d2bb843123013e405ceae9de9110def850fde54
Abdullah Koyuncu <wisewebworks@outlook.com>
2025-06-29 15:41:31 +0200

    Migrate CRA App to Vite

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import Header from './components/header/Header';
 import LegendContainer from './components/legends/LegendContainer';
 
 function App() {
-  return (
-    <Container className="text-center mt-5">
+   return (
+       <Container className="text-center mt-5">
       <Header />
       <LegendContainer />
     </Container>
-  );
+  )
 }
 
-export default App;
+export default App
